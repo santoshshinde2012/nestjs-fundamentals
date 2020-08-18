80dde97b5a2de64cdecbf49d28007976679ee2b5
santosh shinde <santosh.shinde@wwindia.com>
2020-08-18 23:06:03 +0530

    add Use Transactions

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Connection } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Event } from 'src/events/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
+  
   constructor(
+    private readonly connection: Connection,
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
     @InjectRepository(Flavor)
@@ -81,4 +84,28 @@ export class CoffeesService {
     }
     return this.flavorRepository.create({ name });
   }
+
+  /* CoffeesService - recommendCoffee() addition */
+async recommendCoffee(coffee: Coffee) {
+  const queryRunner = this.connection.createQueryRunner();
+  
+  await queryRunner.connect();
+  await queryRunner.startTransaction(); 
+  try {
+    coffee.recommendations++;
+    const recommendEvent = new Event();
+    recommendEvent.name = "recommend_coffee";
+    recommendEvent.type = "coffee";
+    recommendEvent.payload = { coffeeId: coffee.id };
+  
+    await queryRunner.manager.save(coffee); 
+    await queryRunner.manager.save(recommendEvent);
+    
+    await queryRunner.commitTransaction();
+  } catch (err) {
+    await queryRunner.rollbackTransaction();
+  } finally {
+    await queryRunner.release();
+  }
+}
 }
