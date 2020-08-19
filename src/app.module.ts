3c453962dc4505d54766ca41102e2b7d7d1cd616
santosh shinde <santosh.shinde@wwindia.com>
2020-08-19 18:47:23 +0530

    Using the Protocol decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -55,7 +55,7 @@ import appConfig from './config/app.config';
     //   // autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
     //   // synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
     // }),
-    // CoffeeRatingModule
+    // CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
