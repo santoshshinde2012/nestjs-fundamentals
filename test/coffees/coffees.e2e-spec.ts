cf9e1fee18035c8b3ba03ebb5d477c48f2dea619
santosh shinde <santosh.shinde@wwindia.com>
2020-08-19 20:24:14 +0530

    add e2e  test cases

--- test/coffees/coffees.e2e-spec.ts
+++ test/coffees/coffees.e2e-spec.ts
@@ -1,10 +1,19 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe, HttpStatus } from '@nestjs/common';
 import { TestingModule, Test } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+
   let app: INestApplication;
+  let id: string;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -24,12 +33,54 @@ describe('[Feature] Coffees - /coffees', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    
+    app.useGlobalPipes( // 👈 Adding our Global Pipes that our regular application uses
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
     await app.init();
   });
 
-  it.todo('Create [POST /]');
-  it.todo('Get all [GET /]');
-  it.todo('Get one [GET /:id]');
+  it('Create [POST /]', () => { // 👈 Test for our POST endpoint
+    return request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        const expectedCoffee = jasmine.objectContaining({
+          ...coffee,
+          flavors: jasmine.arrayContaining(
+            coffee.flavors.map(name => jasmine.objectContaining({ name })),
+          ),
+        });
+        expect(body).toEqual(expectedCoffee);
+      });
+  });
+  
+  // 💡 EXERCISE - Finish out the rest of the e2e tests here 💡
+  it('Get all [GET /]', () => {
+    return request(app.getHttpServer())
+      .get('/coffees')
+      .expect(HttpStatus.OK)
+      .then(({ body }) => {
+        console.log(body);
+        expect(body).toBeDefined();
+      });
+  });  
+  it('Get one [GET /:id]', () => {
+    return request(app.getHttpServer())
+    .get(`/coffees/1`)
+    .expect(HttpStatus.OK)
+    .then(({ body }) => {
+      expect(body).toBeDefined();
+    });
+  });
   it.todo('Update one [PATCH /:id]');
   it.todo('Delete one [DELETE /:id]');
 
