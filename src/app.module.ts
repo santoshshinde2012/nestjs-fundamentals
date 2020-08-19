a8303174b7fdd80a9973a19a4fc5d7dc84f1d4fc
santosh shinde <santosh.shinde@wwindia.com>
2020-08-19 12:10:40 +0530

    add env validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,17 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import Joi = require('@hapi/joi');
 
 @Module({
   imports: [
     ConfigModule.forRoot(
       {
-        envFilePath: ".environment",
-        // ignoreEnvFile: true,
+        envFilePath: '.environment',
+        validationSchema: Joi.object({
+          DATABASE_HOST: Joi.required(),
+          DATABASE_PORT: Joi.number().default(5432),
+        }),
       }
     ),
     CoffeesModule,
@@ -25,9 +29,16 @@ import { DatabaseModule } from './database/database.module';
       database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
+      // type: 'postgres', // type of our database
+      // host: 'localhost', // database host
+      // port: 5432, // database host
+      // username: 'postgres', // username
+      // password: 'pass123', // user password
+      // database: 'postgres', // name of our database,
+      // autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+      // synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
     }),
-    CoffeeRatingModule,
-    DatabaseModule,
+    CoffeeRatingModule
   ],
   controllers: [AppController],
   providers: [AppService],
