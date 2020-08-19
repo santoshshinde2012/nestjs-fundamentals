90de1c6a7458089353669a33737a174809e56c25
santosh shinde <santosh.shinde@wwindia.com>
2020-08-19 19:03:55 +0530

    Enabling CLI Plugin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { from } from 'rxjs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
@@ -16,12 +18,24 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }));
-  // app.useGlobalFilters(new HttpExceptionFilter());
-  // app.useGlobalGuards(new ApiKeyGuard());
+  
   app.useGlobalInterceptors(
     new WrapResponseInterceptor(), 
     new TimeoutInterceptor(),
   );
+
+  // Setting up Swagger document 
+  const options = new DocumentBuilder()
+  .setTitle('Iluvcoffee')
+  .setDescription('Coffee application')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
