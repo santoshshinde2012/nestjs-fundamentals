737808f01cefe469e0414f5b4891533d3b5b3819
santosh shinde <santosh.shinde@wwindia.com>
2020-08-19 19:06:29 +0530

    Decorating Model Properties

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,11 @@
 import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateCoffeeDto {
+    /**
+     * @ApiProperty decorator useful to *override* 
+     * information automatically inferred from the @nestjs/swagger plugin
+     */
+    @ApiProperty({ description: 'The name of a coffee.' })
     @IsString()
     readonly name: string;
 
